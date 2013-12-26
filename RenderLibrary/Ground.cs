f247d8e378547b5178d09b2bead81ee13859a2fc
msvwesterlund@hotmail.com <msvwesterlund@hotmail.com@140bc737-fe94-0763-7bb6-0a3311148304>
2013-12-26 10:37:06 +0000

    added library

---
+++ RenderLibrary/Ground.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Laboration3Datorgrafik
+{
+    public class Ground
+    {
+        GraphicsDevice device;
+        VertexBuffer groundBuffer;
+
+        public const int groundWidth = 50;
+        public const int groundHeight = 50;
+        Color[] floorColors = new Color[2] { Color.White, Color.Black };
+
+        public Ground(GraphicsDevice device)
+        {
+            this.device = device;
+            BuildFloorBuffer();
+        }
+
+        public void BuildFloorBuffer()
+        {
+            List<VertexPositionColor> groundVertices = new List<VertexPositionColor>();
+
+            int counter = 0;
+
+            for (int x = 0; x < groundWidth; x++)
+            {
+                counter++;
+                for (int z = 0; z < groundHeight; z++)
+                {
+                    counter++;
+                    foreach (VertexPositionColor vertex in FloorTile(x, z, floorColors[counter % 2]))
+                    {
+                        groundVertices.Add(vertex);
+                    }
+                }
+            }
+
+            groundBuffer = new VertexBuffer(device, typeof(VertexPositionColor), groundVertices.Count, BufferUsage.WriteOnly);
+            groundBuffer.SetData<VertexPositionColor>(groundVertices.ToArray());
+        }
+
+        private List<VertexPositionColor> FloorTile(int xOffset, int zOffset, Color tileColor)
+        {
+            List<VertexPositionColor> vList = new List<VertexPositionColor>();
+
+            vList.Add(new VertexPositionColor(new Vector3(0 + xOffset, 0, 0 + zOffset), tileColor));
+            vList.Add(new VertexPositionColor(new Vector3(1 + xOffset, 0, 0 + zOffset), tileColor));
+            vList.Add(new VertexPositionColor(new Vector3(0 + xOffset, 0, 1 + zOffset), tileColor));
+            vList.Add(new VertexPositionColor(new Vector3(1 + xOffset, 0, 0 + zOffset), tileColor));
+            vList.Add(new VertexPositionColor(new Vector3(1 + xOffset, 0, 1 + zOffset), tileColor));
+            vList.Add(new VertexPositionColor(new Vector3(0 + xOffset, 0, 1 + zOffset), tileColor));
+
+            return vList;
+        }
+
+        public void Draw(Effect effect)
+        {
+            //effect.VertexColorEnabled = true;
+            //effect.World = Matrix.Identity;
+
+            //effect.Parameters["xView"].SetValue(ViewMatrix);
+            //effect.Parameters["xProjection"].SetValue(ProjectionMatrix);
+            //effect.Parameters["xWorld"].SetValue(Matrix.Identity);
+
+
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.SetVertexBuffer(groundBuffer);
+                device.DrawPrimitives(PrimitiveType.TriangleList, 0, groundBuffer.VertexCount / 3);
+            }
+        }
+    }
+}
