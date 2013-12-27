23a18a6bc3da9d3cb368b4395f96289610cf9eb9
Martin.Kerrai.Nilsson@gmail.com <Martin.Kerrai.Nilsson@gmail.com@140bc737-fe94-0763-7bb6-0a3311148304>
2013-12-27 13:47:18 +0000

    Fancy Camera, all directions is very nice

--- RenderLibrary/Camera.cs
+++ RenderLibrary/Camera.cs
@@ -16,7 +16,9 @@ namespace Laboration3Datorgrafik
 
         public Matrix ViewMatrix { get; set; }
         public Matrix ProjectionMatrix { get; set; }
-
+        public Quaternion Rotation { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 UpDirection { get; set; }
         public Vector3 CameraPos { get; set; }
 
         public float AspectRatio { get; set; }
@@ -36,5 +38,25 @@ namespace Laboration3Datorgrafik
             this.ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
                 MathHelper.PiOver4, this.AspectRatio, this.nearPlaneDistance, this.farPlaneDistance);
         }
+
+        public void Update(Vector3 pos, Quaternion rot)
+        {
+            this.Rotation = Quaternion.Lerp(this.Rotation, rot, 0.1f);
+
+            Vector3 campos = new Vector3(0, 0, -1f);
+            campos = Vector3.Transform(campos, Matrix.CreateFromQuaternion(this.Rotation));
+            campos += pos;
+
+            Vector3 camup = new Vector3(0, 1, 0);
+            camup = Vector3.Transform(camup, Matrix.CreateFromQuaternion(this.Rotation));
+
+            this.Position = campos;
+            this.UpDirection = camup;
+
+            this.ViewMatrix = Matrix.CreateLookAt(this.Position, pos, this.UpDirection);
+            this.ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4, this.AspectRatio, this.nearPlaneDistance, this.farPlaneDistance);
+        }
+
     }
 }
