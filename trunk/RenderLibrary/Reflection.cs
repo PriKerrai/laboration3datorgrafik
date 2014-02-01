0c779d97aacd41181bc6d428d1467a9247058c8f
karljsjo@gmail.com <karljsjo@gmail.com@140bc737-fe94-0763-7bb6-0a3311148304>
2014-02-01 17:01:04 +0000

    cleaned code in reflection

--- trunk/RenderLibrary/Reflection.cs
+++ trunk/RenderLibrary/Reflection.cs
@@ -34,29 +34,11 @@ namespace RenderLibrary
             {
                 foreach (ModelMeshPart part in mesh.MeshParts)
                 {
-                    //Vector3 diffuse = ((BasicEffect)part.Effect).DiffuseColor;
-                    //Vector3 specularColor = ((BasicEffect)part.Effect).SpecularColor;
-                    //float specularPower = ((BasicEffect)part.Effect).SpecularPower;
-                    //Texture2D texture = ((BasicEffect)part.Effect).Texture;
-                    //float alpha = ((BasicEffect)part.Effect).Alpha;
-
-                    //part.Effect = effect.Clone();
-
-                    //part.Effect.Parameters["Alpha"].SetValue(alpha);
-                    //part.Effect.Parameters["DiffuseColor"].SetValue(new Vector4(diffuse, 0.0f));
-                    //part.Effect.Parameters["SpecularColor"].SetValue(specularColor);
-                    //part.Effect.Parameters["SpecularPower"].SetValue(specularPower);
-                    //part.Effect.Parameters["ModelTexture"].SetValue(texture);
-                    //effect.Parameters["NormalTextureEnabled"].SetValue(false);
-
                     part.Effect = effect.Clone();
 
                     part.Effect.Parameters["Alpha"].SetValue(1);
                     part.Effect.Parameters["DiffuseColor"].SetValue(new Vector4(new Vector3(0.8f, 0.8f, 0.8f), 0.0f));
-                    part.Effect.Parameters["SpecularColor"].SetValue(new Vector3(0f, 0f, 0f));                        
-                    //part.Effect.Parameters["Shininess"].SetValue(16);
-                    //part.Effect.Parameters["ModelTexture"].SetValue(new Texture2D());
-                    //effect.Parameters["NormalTextureEnabled"].SetValue(false);
+                    part.Effect.Parameters["SpecularColor"].SetValue(new Vector3(0f, 0f, 0f));
                 }
             }
         }
@@ -71,37 +53,31 @@ namespace RenderLibrary
 
             graphics.SetRenderTarget(cube, CubeMapFace.PositiveZ);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Backward, Vector3.Up));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Backward, Vector3.Up);
             renderManager.Draw(camera);
 
             graphics.SetRenderTarget(cube, CubeMapFace.NegativeZ);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Forward, Vector3.Up));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Forward, Vector3.Up);
             renderManager.Draw(camera);
 
             graphics.SetRenderTarget(cube, CubeMapFace.NegativeX);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Left, Vector3.Up));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Left, Vector3.Up);
             renderManager.Draw(camera);
 
             graphics.SetRenderTarget(cube, CubeMapFace.PositiveX);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Right, Vector3.Up));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Right, Vector3.Up);
             renderManager.Draw(camera);
 
             graphics.SetRenderTarget(cube, CubeMapFace.PositiveY);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Up, Vector3.Forward));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Up, Vector3.Forward);
             renderManager.Draw(camera);
 
             graphics.SetRenderTarget(cube, CubeMapFace.NegativeY);
             graphics.Clear(Color.Gray);
-            //effect.Parameters["View"].SetValue(Matrix.CreateLookAt(position, position + Vector3.Down, Vector3.Backward));
             camera.ViewMatrix = Matrix.CreateLookAt(position, position + Vector3.Down, Vector3.Backward);
             renderManager.Draw(camera);
 
